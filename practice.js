8f548cd4f4723086d742c72b80891d77049f5a59
nbourisE <nick@hagglr.com>
2016-06-06 19:40:20 +1000

    arb commit

---
+++ practice.js
@@ -0,0 +1,23 @@
+if (true) {
+  console.log("Foo")
+}
+
+else {
+  console.log("Bar")
+}
+if (5>10) {
+  console.log("Nick")
+}
+else {
+  console.log("Steve")
+}
+if (5>10) {
+  console.log("Johnny")
+}
+else if (4>8) {
+  console.log("Tony")
+}
+else {
+  console.log("Seb")
+}
+5>10? console.log("Jimmy") : console.log("Alex")
